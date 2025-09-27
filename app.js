e45bbdc09f66c2fbae47011173dd7f135d0651ff
maynor-lopez <maynor94l39@gmail.com>
2025-09-26 19:32:59 -0600

    Fixing class names, adding regex, removing input submit and change inline styles with classList.add

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
 const form = document.getElementById('form');
 const emailInput = document.querySelector('.hero__input');
-const errorText = document.querySelector('.error__text')
+const errorText = document.querySelector('.error__paragraph')
 
 form.addEventListener('submit', (event) =>{
     event.preventDefault();
     let validSubmit = true;
-    if(emailInput.value.trim() === ''){
-        emailInput.classList.add('input__error');
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if(!emailRegex.test(emailInput.value.trim())){
+        emailInput.classList.add('is-invalid');
         emailInput.placeholder = 'example@email.com';
-        errorText.classList.remove('hide');
+        errorText.classList.remove('error-text');
         validSubmit = false;
     } else{
-        emailInput.classList.remove('input__error');
-        errorText.classList.add('hide');
-        emailInput.style.margin = '0px'
+        emailInput.classList.remove('is-invalid');
+        errorText.classList.add('error-text');
+        emailInput.classList.add('no-margin');
     }
     if(validSubmit){
-        form.submit();
         emailInput.value = '';
         emailInput.placeholder = 'Your email address...'
     }
